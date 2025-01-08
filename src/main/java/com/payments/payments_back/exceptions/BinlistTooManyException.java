b83f35ab5bf9fd42c22d596428bf0926124167e0
Nikhil Pareek <31257061+NikhilPareek88@users.noreply.github.com>
2025-01-08 12:03:16 +0100

    Update BinlistTooManyException.java

--- src/main/java/com/payments/payments_back/exceptions/BinlistTooManyException.java
+++ src/main/java/com/payments/payments_back/exceptions/BinlistTooManyException.java
@@ -1,6 +1,6 @@
 package com.payments.payments_back.exceptions;
 
-public class BinlistTooManyException extends Exception {
+public class BinlistTooManyException extends RuntimeException {
     public BinlistTooManyException(String message) {
         super(message);
     }
